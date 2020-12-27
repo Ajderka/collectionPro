198b292efad6782fb9d0e56671648e6d2019c56e
Айдер <emage.haf@gmail.com>
2020-12-27 21:20:50 +0200

    5.2.2. Реализовать Store<T extends Base> [#148100]

---
+++ src/main/java/generic/store/RoleStore.java
@@ -0,0 +1,42 @@
+package generic.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class RoleStore<T extends Base> implements Store<T> {
+
+    private final List<T> mem = new ArrayList<>();
+
+    @Override
+    public void add(T model) {
+        mem.add(model);
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+        boolean res = false;
+        if (mem.get(Integer.parseInt(id)) != null) {
+            mem.set(Integer.parseInt(id), model);
+            res = true;
+        }
+        return res;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        boolean res = false;
+        if (mem.get(Integer.parseInt(id)) != null) {
+            mem.remove(Integer.parseInt(id));
+            res = true;
+        }
+        return res;
+    }
+
+    @Override
+    public T findById(String id) {
+        if (mem.get(Integer.parseInt(id)) != null) {
+            return mem.get(Integer.parseInt(id));
+        }
+        return null;
+    }
+}
---
+++ src/main/java/generic/store/User.java
@@ -0,0 +1,12 @@
+package generic.store;
+
+public class User extends Base {
+    protected User(String id) {
+        super(id);
+    }
+
+    @Override
+    public String getId() {
+        return super.getId();
+    }
+}
